723d0fe3ea07b295d77f38cee3f27319236c2531
Ashish Thapa <aacict@gmail.com>
2024-11-04 21:52:17 -0500

    verticle name component

---
+++ src/components/common/PageName.tsx
@@ -0,0 +1,14 @@
+
+function PageName({ name }) {
+    return (
+        <div className="flex items-center h-screen justify-center bg-cover bg-center">
+            <div className="flex flex-col items-center">
+                <span className="border-l border-white h-20 mb-2"></span>
+                <p className="text-white font-bold text-lg tracking-widest transform rotate-90">{name}</p>
+                <span className="border-l border-white h-20 mt-2"></span>
+            </div>
+        </div >
+    )
+}
+
+export default PageName
