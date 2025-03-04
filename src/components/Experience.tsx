696d003efffabf0abd59e01b2a364a50ed769a23
Ashish Thapa <aacict@gmail.com>
2025-03-04 00:46:09 -0500

    flexing issue fix with experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -29,23 +29,24 @@ function Experience() {
         <section className={`${isSidebarOpen ? "w-10/12" : "w-full"} section-conatiner flex`} id="Experience">
             <PageName name="Experience" />
             <div className="container all-section-container m-auto px-24" >
-                <div className="flex justify-around items-center space-x-4">
+                <div className="flex flex-wrap">
                     {Experiences.map((exp, index) => (
-                        <div
-                            key={index}
-                            className="shadow-lg p-6 flex-1 text-center bg-gray-500 opacity-65 hover:opacity-100 h-64"
-                        >
-                            <h3 className="font-semibold text-xl">{exp.company}</h3>
-                            <p className="text-black-100 text-xs font-italic">{exp.timeframe}</p>
-                            <p className="text-black-600 pt-4">{exp.position}</p>
-                            <p className="text-black-500">{exp.location}</p>
-                            <div className="relative top-16">
-                                <button
-                                    onClick={() => toggleModal(exp.responsibilities, exp.projects, exp.company)}
-                                    className="bg-green-500 text-gray"
-                                >
-                                    View More
-                                </button>
+                        <div className="p-4" key={index}>
+                            <div
+                                className=" h-64 w-72 p-6 shadow-lg text-center bg-gray-500 opacity-65 hover:opacity-100 "
+                            >
+                                <h3 className="font-semibold text-xl">{exp.company}</h3>
+                                <p className="text-black-100 text-xs font-italic">{exp.timeframe}</p>
+                                <p className="text-black-600 pt-4">{exp.position}</p>
+                                <p className="text-black-500">{exp.location}</p>
+                                <div className="relative top-16">
+                                    <button
+                                        onClick={() => toggleModal(exp.responsibilities, exp.projects, exp.company)}
+                                        className="bg-green-500 text-gray"
+                                    >
+                                        View More
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     ))}
