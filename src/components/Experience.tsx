69a602956b7728d3d5291e49a0957c748d813da3
Ashish Thapa <aacict@gmail.com>
2025-03-04 12:47:34 -0500

    experience added some icons

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -4,6 +4,8 @@ import NavDots from "./common/NavDots"
 import PageName from "./common/PageName"
 import ExperienceModal from "./Modal/ExperienceModal"
 import { useSelector } from "react-redux"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBuilding, faCalendar, faInfoCircle, faMapPin } from "@fortawesome/free-solid-svg-icons"
 
 interface Project {
     name: string;
@@ -29,22 +31,22 @@ function Experience() {
         <section className={`${isSidebarOpen ? "w-10/12" : "w-full"} section-conatiner flex`} id="Experience">
             <PageName name="Experience" />
             <div className="container all-section-container m-auto px-24" >
-                <div className="flex flex-wrap">
+                <div className="flex">
                     {Experiences.map((exp, index) => (
                         <div className="p-4" key={index}>
                             <div
-                                className=" h-64 w-72 p-6 shadow-lg text-center bg-gray-500 opacity-65 hover:opacity-100 "
+                                className=" h-64 w-72 p-6 shadow-lg text-center bg-gray-500 opacity-60 hover:opacity-100 "
                             >
-                                <h3 className="font-semibold text-xl">{exp.company}</h3>
-                                <p className="text-black-100 text-xs font-italic">{exp.timeframe}</p>
+                                <h3 className="font-semibold">{exp.company} <FontAwesomeIcon icon={faBuilding} /></h3>
+                                <p className="text-black-100 font-italic"><FontAwesomeIcon icon={faCalendar} /> {exp.timeframe}</p>
                                 <p className="text-black-600 pt-4">{exp.position}</p>
-                                <p className="text-black-500">{exp.location}</p>
+                                <p className="text-black-500"><FontAwesomeIcon icon={faMapPin} /> {exp.location}</p>
                                 <div className="relative top-16">
                                     <button
                                         onClick={() => toggleModal(exp.responsibilities, exp.projects, exp.company)}
                                         className="bg-green-500 text-gray"
                                     >
-                                        View More
+                                        <FontAwesomeIcon icon={faInfoCircle} /> More
                                     </button>
                                 </div>
                             </div>
