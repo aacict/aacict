038ceb28822d703215ec308c9ae54c0252c57197
Ashish Thapa <aacict@gmail.com>
2025-03-05 10:59:54 -0500

    animation added in experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -31,32 +31,37 @@ function Experience() {
         <section className={`${isSidebarOpen ? "w-10/12" : "w-full"} section-conatiner flex`} id="Experience">
             <PageName name="Experience" />
             <div className="container all-section-container m-auto px-24" >
-                <div className="flex">
-                    {Experiences.map((exp, index) => (
-                        <div className="p-4" key={index}>
-                            <div
-                                className=" h-64 w-72 p-6 shadow-lg text-center bg-gray-500 opacity-60 hover:opacity-100 "
-                            >
-                                <h3 className="font-semibold">{exp.company} <FontAwesomeIcon icon={faBuilding} /></h3>
-                                <p className="text-black-100 font-italic"><FontAwesomeIcon icon={faCalendar} /> {exp.timeframe}</p>
-                                <p className="text-black-600 pt-4">{exp.position}</p>
-                                <p className="text-black-500"><FontAwesomeIcon icon={faMapPin} /> {exp.location}</p>
-                                <div className="relative top-16">
-                                    <button
-                                        onClick={() => toggleModal(exp.responsibilities, exp.projects, exp.company)}
-                                        className="bg-green-500 text-gray"
-                                    >
-                                        <FontAwesomeIcon icon={faInfoCircle} /> More
-                                    </button>
+                <div className="bg-white w-full h-1"></div>
+                <div className="relative w-full overflow-hidden">
+                    <div className="flex animate-scroll">
+                        {Experiences.map((exp, index) => (
+                            <div key={index} className={`p-4`}>
+                                <div className="w-4 h-4 rounded-full relative left-1/2 bottom-2 border-2 border-white"></div>
+
+                                <div
+                                    className=" h-64 w-72 p-6 shadow-lg text-center bg-gray-500 opacity-60 hover:opacity-100 "
+                                >
+                                    <h3 className="font-semibold">{exp.company} <FontAwesomeIcon icon={faBuilding} /></h3>
+                                    <p className="text-black-100 font-italic"><FontAwesomeIcon icon={faCalendar} /> {exp.timeframe}</p>
+                                    <p className="text-black-600 pt-4">{exp.position}</p>
+                                    <p className="text-black-500"><FontAwesomeIcon icon={faMapPin} /> {exp.location}</p>
+                                    <div className="relative top-16">
+                                        <button
+                                            onClick={() => toggleModal(exp.responsibilities, exp.projects, exp.company)}
+                                            className="bg-green-500 text-gray"
+                                        >
+                                            <FontAwesomeIcon icon={faInfoCircle} /> More
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
             </div>
             <NavDots currentPage="Experience" />
             <ExperienceModal isOpen={isOpen} companyName={companyName} toggleModal={toggleModal} responsibilties={responsibilities} projects={projects} />
-        </section>
+        </section >
     )
 }
 
